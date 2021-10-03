caa01255ba356147154ab0e56e95047d1480ab0b
SKJ <sripal.17jccs069@jietjodhpur.ac.in>
2021-10-03 17:33:05 +0530

    Added Python Questions

---
+++ question.py
@@ -0,0 +1,7 @@
+# Ques 1) Filter Odd & Even Number from the Number List.
+
+# Ques 2) A List Contains Numbers & String, Filter Numbers & String in seperate List.
+
+# Ques 3) Find Sum of all Odd Number till N
+
+# Ques 4) Find Sum of all Even Number till N
